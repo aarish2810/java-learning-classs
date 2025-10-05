4354bb052d34cf36337b81dc2a28434afa2496f3
aariaarish <aarisha_b@yahoo.com>
2025-10-05 01:50:15 -0400

    commit day2

---
+++ src/main/java/Test1.java
@@ -0,0 +1,9 @@
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("Hi world");
+    }
+}
+//Hungarian - MyNameIsJava
+//camelcase - myNameIsJava
+
+//java first letter [A-Za-z$_]
---
+++ src/main/java/VariablesDemo.java
@@ -0,0 +1,17 @@
+public class VariablesDemo {
+    public static void main(String[] args) {
+        int a=10;
+        final int b= 20;
+       // System.out.println(a);
+
+        int c=8;
+        int r= 10;
+         c=c+r;
+         r=c-r;
+         c=c-r;
+        System.out.println(r);
+        System.out.println(c);
+
+
+    }
+}
