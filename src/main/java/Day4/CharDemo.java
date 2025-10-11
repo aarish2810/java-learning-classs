9aa24420a986cd83111e4053743342b641bc4db5
aariaarish <aarisha_b@yahoo.com>
2025-10-11 02:28:48 -0400

    update for unicode

--- src/main/java/Day4/CharDemo.java
+++ src/main/java/Day4/CharDemo.java
@@ -1,11 +1,13 @@
 package Day4;
 
+import java.sql.SQLOutput;
+
 public class CharDemo {
     public static void main(String[] args) {
         //2 types - ASCII & Unicode
         //ASCII - 1 byte
 
-        char ch='u';
+       /* char ch='u';
         System.out.println(ch);
 
         int e=65;
@@ -13,17 +15,18 @@ public class CharDemo {
         System.out.println(c);
 
         char c1='S'+8;
-        System.out.println(c1);
+        System.out.println(c1);*/
 
         // Unicode - 2 byte
-
+        System.out.println("my son name in tamil:");
         char ni='\u0BA8';
         char as='\u0BBF';
         char la='\u0BB2';
         char in ='\u0BA9';
         char in2='\u0BCD';
 
-        System.out.println(""+ni+as+la+in+in2);
+
+
 
 
 
