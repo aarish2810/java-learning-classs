2b812c66064485b85b794b552f500c30d6fd4639
aariaarish <aarisha_b@yahoo.com>
2025-10-11 01:41:13 -0400

    update all day 4

--- src/main/java/Day4/NumberDataType.java
+++ src/main/java/Day4/NumberDataType.java
@@ -2,7 +2,7 @@ package Day4;
 
 public class NumberDataType {
     public static void main(String[] args) {
-        long creditcard=5678_8799_5788_890l;
+        long creditcard= 5678_8799_5788_890L;
         System.out.println(creditcard);
 
 
