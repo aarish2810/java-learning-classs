2f292d0eb5be53cba62d2a8774122874c81773e9
aariaarish <aarisha_b@yahoo.com>
2025-10-13 01:03:12 -0400

    update for ConvertInches

---
+++ src/main/java/Assignment/ConvertInches.java
@@ -0,0 +1,23 @@
+package Assignment;
+
+import java.util.Scanner;
+
+public class ConvertInches {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Provide inches:");
+      //  double inches = sc.nextDouble();
+
+        if (sc.hasNextDouble()) {
+            double inches = sc.nextDouble();
+            double metres = inches * 0.0254;
+
+            System.out.printf("In meters: %.2f", metres);
+        }
+        else  {
+            System.out.println("Invalid input");
+        }
+        sc.close();
+    }
+}
---
+++ src/main/java/Assignment/ShiftOp.java
@@ -0,0 +1,16 @@
+package Assignment;
+
+public class ShiftOp {
+    public static void main(String[] args) {
+
+      //  int i=-10;
+      //System.out.println(i>>>2);
+        //System.out.println(i>>>30);
+      //  System.out.println(i>>>32);
+        int j=10;
+        System.out.println(j>>1);
+        System.out.println(j<<1);
+        System.out.println(j>>>1);
+
+    }
+}
